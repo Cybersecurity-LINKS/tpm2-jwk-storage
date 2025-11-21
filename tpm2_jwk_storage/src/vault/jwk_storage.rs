673bf87d2a75d74dd196bee273877144f6708915
Michele Festa <michele.festa@linksfoundation.com>
2025-11-21 17:39:24 +0100

    0 warnings

--- tpm2_jwk_storage/src/vault/jwk_storage.rs
+++ tpm2_jwk_storage/src/vault/jwk_storage.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::time::Instant;
-
 use async_trait::async_trait;
 use identity_iota::{storage::{JwkGenOutput, JwkStorage, KeyId, KeyStorageError, KeyStorageErrorKind, KeyStorageResult, KeyType}, verification::{jwk::Jwk, jws::JwsAlgorithm, jwu::{decode_b64, encode_b64}}};
 
